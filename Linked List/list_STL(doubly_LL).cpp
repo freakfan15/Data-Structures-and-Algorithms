099996536a319551b0a15e5fdbe8d2ef34132095
freakfan15 <vivekkrrai.bxr@gmail.com>
2020-10-10 19:51:57 +0530

    List STL(Doubly LL)

--- Linked List/list_STL(doubly_LL).cpp
+++ Linked List/list_STL(doubly_LL).cpp
@@ -13,10 +13,31 @@ int main(int argc, char const *argv[])
 	list<string> l2{"apple", "banana", "mango", "guava"};
 	l2.push_back("pineapple");
 
+	//sort
+	l2.sort(); //l2 is sorted lexigraphically
+
 	//iterate over the list and print teh data
 	for(string  s:l2){
 		cout<<s<<"->";
 	}
+	//some operations
+	cout<<endl<<l2.front()<<endl;
+
+	l2.pop_back();
+	l2.push_front("kiwi");
+
+	//print using iterator
+	for(auto it=l2.begin(); it!=l2.end(); it++){
+		cout<<(*it)<<" -> ";
+	}
+
+	cout<<endl<<"reversing the list: ";
+	l2.reverse();
+	for(auto it=l2.begin(); it!=l2.end(); it++){
+		cout<<(*it)<<" -> ";
+	}
+
+
 
 
 
