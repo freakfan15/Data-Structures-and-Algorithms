caa5f856eec59de4bcb96fe74265cdc1d2760eae
freakfan15 <vivekkrrai.bxr@gmail.com>
2020-12-11 22:22:40 +0530

    Create rotate_And_nextPermutations.cpp
    function to roatate the container about a  certain elemnt and to find next permutations

---
+++ Algorithms STL/rotate_And_nextPermutations.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	//applying rotate function i.e rotate(start, middle, end);
+
+	//Applying on arrays
+	int a[]= {10,20,30,40,50};
+	int n=sizeof(a)/sizeof(int);
+
+	rotate(a, a+2, a+n);
+
+	for(int i=0; i<n; i++){
+		cout<<a[i]<<", ";
+	}
+	cout<<endl;
+	//Applying the same for vectors
+	vector<int> v{10,20,30,40,50};
+	rotate(v.begin(), v.begin()+3, v.end());
+
+	for(auto it:v){
+		cout<<it<<", ";
+	}
+	cout<<endl;
+	//next permutations finds the permutation that is just greater than the current arrangement
+	vector<int> v1{1,2,3};
+	next_permutation(v1.begin(), v1.end());
+
+	for(int x:v1){
+		cout<<x<<" ";
+	}
+	return 0;
+}
